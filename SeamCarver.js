85dc97720b14a3cf6aee9b8dd78a9f5f477f2ca1
Ian Ribas <ianemari@gmail.com>
2016-05-26 23:27:16 -0400

    First forray on seam carving, with finding vertical seams. Horizontal seams could be implemented much the same way. Removing the seam is somewhat more complicated, might need a different datastructure for the energy matrix to be efficient.

--- SeamCarver.js
+++ SeamCarver.js
@@ -1,18 +1,33 @@
 "use strict";
 
+const RED = 0;
+const GREEN = 1;
+const BLUE = 2;
+const BORDER_ENERGY = 1000;
+
 class SeamCarver {
     constructor(canvas) {
         this.width = canvas.width;
         this.height = canvas.height;
         this.picture = [];
-        var context = canvas.getContext("2d");
+        this.context = canvas.getContext("2d");
         for (var y = 0; y < this.height; y ++) {
             for (var x = 0; x < this.width; x ++) {
-                var color = context.getImageData(x, y, 1, 1).data;
+                var color = this.context.getImageData(x, y, 1, 1).data;
                 var rgb = this.rgbToNum(color[0], color[1], color[2]);
                 this.picture[this.pixelToIndex(x, y)] = rgb;
             }
         }
+
+        // Simple implementation of energy matrix as array of arrays.
+        // Because we need to remove items, when removing the seam,
+        // maybe some sort of linked structure is more efficient.
+        this.energy_matrix = new Array(this.width);
+        for (var i = 0; i < this.width; i++) {
+            this.energy_matrix[i] = new Array(this.height);
+        }
+
+        this.createEnergyMatrix();
     }
 
     pixelToIndex(x, y) {
@@ -45,7 +60,114 @@ class SeamCarver {
         return [red, green, blue];
     }
 
+    energy(x, y) {
+        if (x <= 0 || y <= 0 || x >= this.width-1 || y >= this.height-1) {
+            return BORDER_ENERGY;
+        }
+        var xant = this.context.getImageData(x - 1, y, 1, 1).data;
+        var xpost = this.context.getImageData(x + 1, y, 1, 1).data;
+        var yant = this.context.getImageData(x, y - 1, 1, 1).data;
+        var ypost = this.context.getImageData(x, y + 1, 1, 1).data;
 
+        return Math.sqrt(
+            (xpost[RED] - xant[RED])*(xpost[RED] - xant[RED]) +
+            (xpost[GREEN] - xant[GREEN])*(xpost[GREEN] - xant[GREEN]) +
+            (xpost[BLUE] - xant[BLUE])*(xpost[BLUE] - xant[BLUE]) +
+            (ypost[RED] - yant[RED])*(ypost[RED] - yant[RED]) +
+            (ypost[GREEN] - yant[GREEN])*(ypost[GREEN] - yant[GREEN]) +
+            (ypost[BLUE] - yant[BLUE])*(ypost[BLUE] - yant[BLUE])
+        );
+    }
+
+    /**
+     * Calculate energy_matrix information for pixel x,y.
+     * Assumes x and y in range.
+    */
+    recalculate(x, y) {
+        var energy_cell = {};
+
+        energy_cell.energy = this.energy(x, y);
+        energy_cell.vminsum = Number.POSITIVE_INFINITY;
+
+        if (y  >= this.height-1) {
+            energy_cell.vminsum = energy_cell.energy;
+            energy_cell.minx = 0;
+        } else {
+            var cursum = 0;
+            var curminx = 0;
+            if (x - 1 >= 0) {
+                energy_cell.vminsum = this.energy_matrix[x - 1][y + 1].vminsum + energy_cell.energy;
+                energy_cell.minx = x - 1;
+            }
+
+            cursum = this.energy_matrix[x][y + 1].vminsum + energy_cell.energy;
+            if (cursum < energy_cell.vminsum) {
+                energy_cell.vminsum = cursum;
+                energy_cell.minx = x;
+            }
+
+            if (x + 1 < this.width) {
+                cursum = this.energy_matrix[x + 1][y + 1].vminsum + energy_cell.energy;
+                if (cursum < energy_cell.vminsum) {
+                    energy_cell.vminsum = cursum;
+                    energy_cell.minx = x + 1;
+                }
+            }
+        }
+
+        return energy_cell;
+    }
+
+    createEnergyMatrix() {
+        // This has to be reverse order (bottom to top)
+        for (var y = this.height; y >= 0; y--) {
+            // This can be in any order ...
+            for (var x = 0; x < this.width; x++) {
+                this.energy_matrix[x][y] = this.recalculate(x,y);
+            }
+        }
+    }
+
+    findVerticalSeam() {
+        var vseam = [];
+
+        var xminsum = 0;
+        var vminsum = Number.POSITIVE_INFINITY;
+
+        // Find smallest sum on first row
+        for (var x = 0; x < this.width; x++) {
+            if (this.energy_matrix[x][0].vminsum < vminsum) {
+                vminsum = this.energy_matrix[x][0].vminsum;
+                xminsum = x;
+            }
+        }
+
+        vseam[0] = xminsum;
+
+        // Follow down to get array
+        var y = 0;
+        while (y < this.height - 1) {
+            xminsum = this.energy_matrix[xminsum][y].minx
+            y++;
+            vseam[y] = xminsum;
+        }
+
+        return vseam;
+    }
+
+    /**
+     * Prints one of the values of the energy_matrix. Useful for debugging.
+    */
+    printMatrix(field) {
+        var line = "";
+        for (var y = 0; y < this.height; y++) {
+            for (var x = 0; x < this.width; x++) {
+                line += this.energy_matrix[x][y][field] + "\t";
+            }
+            console.log(line);
+            line = "";
+        }
+    }
 }
 
 module.exports = SeamCarver;
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var assert = require('assert');
+var assert = require('chai').assert;
 var fs = require('fs');
 var canvas = require('canvas');
 var SeamCarver = require('./SeamCarver.js');
@@ -8,7 +8,7 @@ var convertImageToCanvas = require('./convertImageToCanvas.js');
 
 describe('SeamCarver', function () {
     function getSm(image, fn) {
-        fs.readFile(__dirname + '/images/3x4.png', function(err, image){
+        fs.readFile(__dirname + image, function(err, image){
             if (err) throw err;
             var img = new canvas.Image();
             img.src = image;
@@ -18,12 +18,43 @@ describe('SeamCarver', function () {
     };
 
     it('should set width, height and pixels correctly', function(done) {
-          getSm("/images/3x4.png", function(sm) {
-              assert.equal(sm.width, 3);
-              assert.equal(sm.height, 4);
-              assert.equal(sm.picture.length, 12);
-              assert.equal(sm.picture, [0, 0, 0]);
-              done();
-          });
+        getSm("/images/3x4.png", function(sm) {
+            assert.equal(sm.width, 3);
+            assert.equal(sm.height, 4);
+            assert.equal(sm.picture.length, 12);
+            // assert.equal(sm.picture, [16737587, 16737689, 16737791, 16750899, 16751001, 16751103, 16763699, 16764057, 16764415, 16777011, 16777113, 16777215]);
+            done();
+        });
+    });
+
+    it('should give energy of pixel', function(done) {
+        getSm("/images/3x4.png", function(sm) {
+            assert.equal(sm.energy(0, 0), 1000);
+            assert.equal(sm.energy(0, 3), 1000);
+            assert.equal(sm.energy(2, 0), 1000);
+            assert.equal(sm.energy(2, 3), 1000);
+            assert.equal(sm.energy(1, 2), Math.sqrt(52024));
+            assert.equal(sm.energy(1, 1), Math.sqrt(52225));
+            done();
+        });
+    });
+
+    it('should create an energy matrix to help find seam', function(done) {
+        getSm("/images/3x4.png", function(sm) {
+            assert.equal(sm.energy_matrix[0][3].energy, 1000);
+            assert.equal(sm.energy_matrix[0][3].vminsum, 1000);
+            assert.equal(sm.energy_matrix[0][3].minx, 0);
+            assert.equal(sm.energy_matrix[1][2].energy, Math.sqrt(52024));
+            assert.equal(sm.energy_matrix[1][2].vminsum, 1000 + Math.sqrt(52024));
+            assert.equal(sm.energy_matrix[1][2].minx, 0);
+            done();
+        });
+    });
+
+    it('should calculate vertical seam  an energy matrix to help find seam', function(done) {
+        getSm("/images/6x5.png", function(sm) {
+            assert.deepEqual(sm.findVerticalSeam(), [3, 4, 3, 2, 1]);
+            done();
+        });
     });
 });
